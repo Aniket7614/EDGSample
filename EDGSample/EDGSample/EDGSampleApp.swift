4f905989f6a415de607b273d3d4dd4f34c619910
Aniket7614 <aniketk561@gmail.com>
2023-04-25 18:47:12 +0530

    Add files via upload
    Commit- Initial Set Up

---
+++ EDGSample/EDGSample/EDGSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  EDGSampleApp.swift
+//  EDGSample
+//
+//  Created by Halcyon Tek on 25/04/23.
+//
+
+import SwiftUI
+
+@main
+struct EDGSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
